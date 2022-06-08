6ead08481ddc6f69b8f82f686af9a6bec460cc48
dahye <59674969+dhnjun9@users.noreply.github.com>
2022-06-08 17:30:14 +0900

    컨트롤러 작성 완료

--- src/main/java/application/chattingclient/AppController.java
+++ src/main/java/application/chattingclient/AppController.java
@@ -1,46 +1,116 @@
 package application.chattingclient;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-import java.nio.charset.StandardCharsets;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-import static application.chattingclient.ClientMain.startClient;
-import static application.chattingclient.ClientMain.stopClient;
+public class AppController implements Initializable {
+    @FXML private ToolBar bottomTool;
+    @FXML private Button enterBtn;
+    @FXML private Button sendBtn;
+    @FXML private TextField nameSpace;
+    @FXML private TextField msgText;
+    @FXML private TextArea textArea;
+    Socket socket;
 
-public class AppController {
+    public void startClient(){
+        String serverIp = "127.0.0.1";
+        int port = 1119;
+        Thread thread = new Thread(() -> {
+            try{
+                socket = new Socket(serverIp,port);
+                System.out.println("0");
+                Platform.runLater(()->{
+                    nameSpace.setEditable(false);
+                    bottomTool.setDisable(false);
+                    enterBtn.setText("나가기");
+                });
+                Thread receiver = new Thread(new ClientReceiver(socket));
+                receiver.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+    public void stopClient(){
+        try{
+            if(socket!=null && !socket.isClosed()){
+                socket.close();
+            }
+        } catch (Exception e) {
+            stopClient();
+        }
+    }
+
+    public void send(String msg){
+        Thread thread = new Thread(() -> {
+            try {
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                out.writeUTF(msg);
+            } catch (IOException e) {  }
+        });
+        thread.start();
+    }
+
+    class ClientReceiver implements Runnable{
+
+        Socket socket;
+        DataInputStream in;
+
+        ClientReceiver(Socket socket){
+            this.socket = socket;
+            try{
+                in = new DataInputStream(socket.getInputStream());
+            } catch (IOException e) { }
+        }
+
+        @Override
+        public void run() {
+            while (in!=null){
+                try{
+                    String msg = in.readUTF();
+                    Platform.runLater(()->{
+                        textArea.appendText(msg);
+                    });
+                } catch (IOException e) { }
+            } // while
+        } // run
+    } // ClientReceiver
+
+
     @FXML
-    private ToolBar bottomTool;
-
-    @FXML
-    private Button enterBtn;
-
-    @FXML
-    private Button sendBtn;
-
-    @FXML
-    private TextField nameSpace;
-
-    @FXML
-    private TextField msgText;
-
-    @FXML
-    protected void enterBtnClick() {
-        if(enterBtn.getText()=="들어가기") {
-            String name = nameSpace.getText();
-            startClient(name);
-            bottomTool.setDisable(false);
-            enterBtn.setText("나가기");
+    protected void onEnterBtnClick() {
+        if(enterBtn.getText().equals("들어가기")) {
+            startClient();
         }else{
             stopClient();
+            nameSpace.setEditable(true);
             bottomTool.setDisable(true);
             enterBtn.setText("들어가기");
         }
     }
 
     @FXML
-    protected void sendBtnClick() {
-        msgText.getText();
+    protected void onSendBtnClick() {
+        String name = nameSpace.getText();
+        String txt = msgText.getText();
+        send("["+name+"] "+txt);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        enterBtn.setOnAction(event -> onEnterBtnClick());
+        sendBtn.setOnAction(event -> onSendBtnClick());
     }
 
 }
--- src/main/java/application/chattingclient/ClientMain.java
+++ src/main/java/application/chattingclient/ClientMain.java
@@ -12,94 +12,11 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ClientMain extends Application {
-    static Socket socket;
-
-    public static void startClient(String name){
-        try{
-            String serverIp = "127.0.0.1";
-
-            socket = new Socket(serverIp,1119);
-            System.out.println("연결 완");
-            Thread sender = new Thread(new ClientSender(socket, name));
-            Thread receiver = new Thread(new ClientReceiver(socket));
-            sender.start();
-            receiver.start();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } // try
-    }
-
-    public static void stopClient(){
-        try{
-            if(socket!=null && !socket.isClosed()){
-                socket.close();
-            }
-        } catch (Exception e) {
-            stopClient();
-        }
-    }
-
-    static class ClientReceiver implements Runnable{
-
-        Socket socket;
-        DataInputStream in;
-
-        ClientReceiver(Socket socket){
-            this.socket = socket;
-            try{
-                in = new DataInputStream(socket.getInputStream());
-            } catch (IOException e) { }
-        }
-
-        @Override
-        public void run() {
-            while (in!=null){
-                try{
-                    String msg = in.readUTF();
-                } catch (IOException e) { }
-            } // while
-        } // run
-    } // ClientReceiver
-
-    static class ClientSender implements Runnable{
-
-        Socket socket;
-        DataOutputStream out;
-        String name;
-        String msg;
-
-        ClientSender(Socket socket, String name){
-            this.socket = socket;
-            try {
-                out = new DataOutputStream(socket.getOutputStream());
-                this.name = name;
-            } catch (IOException e) {  }
-        }
-
-        public static void getMsgText(String msg){
-            //this.msg = msg;
-        }
-
-        @Override
-        public void run() {
-            try {
-                if(out!=null){
-                    out.writeUTF(name);
-                }
-
-                while (out!=null){
-                    out.writeUTF("["+name+"]");
-                }
-            } catch (IOException e) { }
-        }
-    }
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientMain.class.getResource("client-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 450);
         stage.setTitle("어쩌구톡");
         stage.setScene(scene);
         stage.show();
